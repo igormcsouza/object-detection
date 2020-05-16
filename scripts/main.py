ae171a19bbfb25867bb23d0c41b6a3c307e75da3
Igor <igormcsouza@gmail.com>
2020-05-15 22:38:12 -0300

    Starting service and api

---
+++ scripts/main.py
@@ -0,0 +1,16 @@
+''' Object Detection
+
+Author: Igor Souza
+Email: igormcsouza@gmail.com
+
+How to use it
+
+...
+'''
+
+import sys
+
+if __name__ == '__main__':
+    # Test mode
+    if sys.argv[1] == 'test':
+        print("It's working properly!!")
